0445940f255e2e817835f811b07563e2e3353c2f
picchi24 <99118481+picchi24@users.noreply.github.com>
2022-03-30 12:04:52 +0600

    Update Psycho.py

--- Psycho.py
+++ Psycho.py
@@ -155,7 +155,7 @@ def main_system():
     print "\x1b[1;91m"
     print 50*'-'
    
-    print "\x1b[1;92m[*]\x1b[1;96m FOR NEED ANY HELP TYPE 7 AND \x1b[1;93mWHATSAPP ME"
+    print "\x1b[1;92m[✓]\x1b[1;96m FOR NEED ANY HELP TYPE 7 AND \x1b[1;93mWHATSAPP ME"
     print "\x1b[1;91m"
     print 50*'-' 
     main_input()
