050586de081bfd63b6de4b83d7ba9bb1bf4424db
picchi24 <99118481+picchi24@users.noreply.github.com>
2022-03-30 12:20:26 +0600

    Update Psycho.py

--- Psycho.py
+++ Psycho.py
@@ -144,7 +144,7 @@ def main_system():
     os.system('clear')
     print logo
     print "" 
-    print \x1b[1;91m
+    print "\x1b[1;91m"
     print 50*'~'
     print "\x1b[1;95m[1] Public Cloning    \x1b[1;90m(Login)"
     print "\x1b[1;92m[2] Random Cloning \x1b[1;90m(No Login)"
